a99df9e7692896c0bf59e97033a666831749def8
Marcdonaldtec <marcdonaldtech40@gmail.com>
2023-12-08 12:55:45 -0500

    this is the home page

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+// Add your custom JavaScript here
+console.log('Script loaded.');
+
+// JavaScript function to toggle the mobile menu
+function toggleMobileMenu() {
+    var mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('active');
+}
